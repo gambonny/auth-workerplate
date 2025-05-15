c8fb85337b20c07b4af160a90ef2b6daaa98b24a
Jonny Gamba <gambonny@gmail.com>
2025-05-15 13:39:38 -0500

    add: basic logger implementation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Hono } from "hono"
+import { createLogger } from "@gambonny/cflo"
+
+const logger = createLogger({
+	level: "info",
+	format: "pretty",
+})
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
 app.post("/signup", c => {
+	logger.debug("This log should not appear.")
 	return c.text("Hello Hono!")
 })
 
